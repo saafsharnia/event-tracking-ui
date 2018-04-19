73bb30868a5073789f01cb7d28045570c3c988fb
saeedeh <saafsharnia@gmail.com>
2018-04-19 20:12:44 +0200

    Setup react-routing

--- src/redux/reducers/combineReducers.js
+++ src/redux/reducers/combineReducers.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import EventsReducer from './EventsReducer';
 import ProjectsReducer from './ProjectsReducer';
+import {routerReducer} from 'react-router-redux';
 
 export default combineReducers({
     events: EventsReducer,
-    projects: ProjectsReducer
+    projects: ProjectsReducer,
+    routing: routerReducer
 });
